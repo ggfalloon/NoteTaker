b02c513b779b36de9943783df464ffb03fccd0a1
Gabriele Falloon <ggfalloon@gmail.com>
2021-01-08 13:35:47 -0600

    moved server.js out of public folder, added path, corrected filenames

--- server.js
+++ server.js
@@ -1,27 +1,43 @@
 // Dependencies =============================================================
 var express = require("express");
+var path = require("path");
 var fs = require("fs");
 
 //  Sets up the Express App  =================================================
 var app = express();
 var PORT = process.env.PORT || 4000;
 
-var notesData = [];
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+// API routes
 
 app.get("/api/notes", function (req, res) {
-    return res.json(notesData);
+
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log(data)
+        var parseData = JSON.parse(data);
+        return res.json(parseData);
+    })
+
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // app.get("/api/waitlist", function (req, res) {
@@ -43,5 +59,9 @@ app.delete("/api/notes/:id", function (req, res) {
 
     }
 })
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
 
 
